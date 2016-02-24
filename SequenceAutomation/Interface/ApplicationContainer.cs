7399c5b54f218eb9c806fd1616f77b07e8688000
Brian Briscoe <brianbriscoe06@gmail.com>
2016-02-24 09:15:48 +0000

    Commenting

--- SequenceAutomation/Interface/ApplicationContainer.cs
+++ SequenceAutomation/Interface/ApplicationContainer.cs
@@ -12,25 +12,49 @@ namespace SequenceAutomation
 {
     public partial class ApplicationContainer : Form
     {
+        /*
+         * Method: ApplicationContainer()
+         * Summary: Class constructor
+         */
         public ApplicationContainer()
         {
             InitializeComponent();
+
+            // Initialising the events to their appropriate methods
             loginUserControl.CreateButtonEvent += gotoCreate;
             loginUserControl.PlayButtonEvent += gotoPlay;
-            createRecUserControl.BackButtonEvent += returnToHome;
-            playRecUserControl.BackButtonEvent += returnToHome;
+            createRecUserControl.BackButtonEvent += returnToLogin;
+            playRecUserControl.BackButtonEvent += returnToLogin;
         }
 
-        private void returnToHome(object sender, EventArgs e)
+        /* 
+         * Method: returnToLogin()
+         * Summary: Brings up the login menu by bringing it to the front of the view
+         * Parameter: sender - The control that the action is for, in this case the button
+         * Parameter: e - Any arguments the function may use
+         */
+        private void returnToLogin(object sender, EventArgs e)
         {
             loginUserControl.BringToFront();
         }
 
+        /* 
+         * Method: gotoPlay() 
+         * Summary: Brings up the play recording menu by bringing it to the front of the view
+         * Parameter: sender - The control that the action is for, in this case the button
+         * Parameter: e - Any arguments the function may use
+         */
         public void gotoPlay(object sender, EventArgs e)
         {
             playRecUserControl.BringToFront();
         }
 
+        /* 
+        * Method: gotoCreate()
+        * Summary: Brings up the create recording menu by bringing it to the front of the view
+        * Parameter: sender - The control that the action is for, in this case the button
+         * Parameter: e - Any arguments the function may use
+        */
         protected void gotoCreate(object sender, EventArgs e)
         {
             createRecUserControl.BringToFront();
