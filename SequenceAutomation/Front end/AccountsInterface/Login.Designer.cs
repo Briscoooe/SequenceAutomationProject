800de6b083e5abbb0837a53bac0a6578d6834fbe
Brian Briscoe <brianbriscoe06@gmail.com>
2016-03-12 20:47:42 +0000

    Added  bones of login interface

---
+++ SequenceAutomation/Front end/AccountsInterface/Login.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SequenceAutomation.Front_end.AccountsInterface
+{
+    partial class Login
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showTutorialBtn = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernameTb = new System.Windows.Forms.RichTextBox();
+            this.passwordTb = new System.Windows.Forms.RichTextBox();
+            this.goBackBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showTutorialBtn
+            // 
+            this.showTutorialBtn.BackColor = System.Drawing.Color.White;
+            this.showTutorialBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.showTutorialBtn.FlatAppearance.BorderSize = 3;
+            this.showTutorialBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.showTutorialBtn.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showTutorialBtn.Location = new System.Drawing.Point(214, 291);
+            this.showTutorialBtn.Name = "showTutorialBtn";
+            this.showTutorialBtn.Size = new System.Drawing.Size(196, 60);
+            this.showTutorialBtn.TabIndex = 88;
+            this.showTutorialBtn.Text = "Login";
+            this.showTutorialBtn.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(125, 239);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(285, 36);
+            this.checkBox1.TabIndex = 87;
+            this.checkBox1.Text = "Keep me logged in";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(190, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 41);
+            this.label4.TabIndex = 86;
+            this.label4.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(304, 37);
+            this.label3.TabIndex = 85;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(350, 33);
+            this.label2.TabIndex = 84;
+            this.label2.Text = "Username or email adress";
+            // 
+            // usernameTb
+            // 
+            this.usernameTb.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTb.Location = new System.Drawing.Point(36, 118);
+            this.usernameTb.Name = "usernameTb";
+            this.usernameTb.Size = new System.Drawing.Size(374, 31);
+            this.usernameTb.TabIndex = 82;
+            this.usernameTb.Text = "";
+            // 
+            // passwordTb
+            // 
+            this.passwordTb.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTb.Location = new System.Drawing.Point(34, 201);
+            this.passwordTb.Name = "passwordTb";
+            this.passwordTb.Size = new System.Drawing.Size(376, 34);
+            this.passwordTb.TabIndex = 83;
+            this.passwordTb.Text = "";
+            // 
+            // goBackBtn
+            // 
+            this.goBackBtn.BackgroundImage = global::SequenceAutomation.Properties.Resources.backbutton;
+            this.goBackBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.goBackBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.goBackBtn.FlatAppearance.BorderSize = 0;
+            this.goBackBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.goBackBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.goBackBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goBackBtn.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goBackBtn.Location = new System.Drawing.Point(2, 3);
+            this.goBackBtn.Name = "goBackBtn";
+            this.goBackBtn.Size = new System.Drawing.Size(169, 73);
+            this.goBackBtn.TabIndex = 89;
+            this.goBackBtn.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.Controls.Add(this.goBackBtn);
+            this.Controls.Add(this.showTutorialBtn);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usernameTb);
+            this.Controls.Add(this.passwordTb);
+            this.Name = "Login";
+            this.Size = new System.Drawing.Size(583, 378);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goBackBtn;
+        private System.Windows.Forms.Button showTutorialBtn;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox usernameTb;
+        private System.Windows.Forms.RichTextBox passwordTb;
+    }
+}
---
+++ SequenceAutomation/Front end/AccountsInterface/Options.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SequenceAutomation.Front_end.AccountsInterface
+{
+    partial class Options
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(191, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 41);
+            this.label4.TabIndex = 78;
+            this.label4.Text = "Login";
+            // 
+            // Options
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.Controls.Add(this.label4);
+            this.Name = "Options";
+            this.Size = new System.Drawing.Size(860, 389);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+    }
+}
